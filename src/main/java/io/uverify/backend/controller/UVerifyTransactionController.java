68227298aeea993f9a554355f531ba230f1e6d3e
fabianbormann <fabian.bormann@cardanofoundation.org>
2026-03-05 22:44:48 +0100

    fix: repair broken import

--- src/main/java/io/uverify/backend/controller/UVerifyTransactionController.java
+++ src/main/java/io/uverify/backend/controller/UVerifyTransactionController.java
@@ -18,6 +18,7 @@
 
 package io.uverify.backend.controller;
 
+import com.bloxbean.cardano.client.api.model.Result;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -33,12 +34,7 @@ import io.uverify.backend.enums.TransactionType;
 import io.uverify.backend.service.UVerifyTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @SuppressWarnings("unused")
